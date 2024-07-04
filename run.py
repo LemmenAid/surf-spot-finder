18090a5df36bf0387657da0f0c35bce5e35b5724
Karlan Lemmen <karlanlemmen@gmail.com>
2024-07-04 14:40:55 +0000

    Wire up APIs and confirm access to google sheet

--- run.py
+++ run.py
@@ -16,28 +16,4 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('surf_spot_finder')
 
 
-def get_counties():
-    """
-    Retrieve the list of available counties from the first worksheet
-    """
-    counties_sheet = SHEET.get_worksheet(0)
-    return counties_sheet.row_values(1)
-    
 
-def main():
-    print("Welcome to the Irish Surf Spot Finder!\n")
-    print("We want to help you find the best surfspots in Ireland.")
-    print("First thing we need to know to help you on your way is in which County you would like to go surfing..\n")
-
-    # Display the list of available counties
-    counties = get_counties()
-    print("Here is a list of the available counties:\n")
-    for county in counties:
-        print(f"- {county}\n")
-
-    # Prompt the user to choose a county
-    selected_county = input("Enter the County you want to explore: ")
-
-  
-if __name__ == '__main__':
-    main()
